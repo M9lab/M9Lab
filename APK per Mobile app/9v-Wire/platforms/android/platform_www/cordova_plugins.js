fae13fd74e0f9a6938b4a8d279a84fa6a238c032
Stefano Ferrara <stefano.ferrara@sede.crowdm.it>
2019-03-12 17:46:42 +0100

    App mobile 9vWire

---
+++ APK per Mobile app/9v-Wire/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-bluetooth-serial.bluetoothSerial",
+    "file": "plugins/cordova-plugin-bluetooth-serial/www/bluetoothSerial.js",
+    "pluginId": "cordova-plugin-bluetooth-serial",
+    "clobbers": [
+      "window.bluetoothSerial"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.2",
+  "cordova-plugin-bluetooth-serial": "0.4.7"
+};
+// BOTTOM OF METADATA
+});
