659525164ef1dd7d99dcda73a73b08d35d3c402b
stefano.ferrara@crowdm.it <stefano.ferrara@crowdm.it>
2025-12-16 21:21:05 +0100

    test text tft ok

--- Code/arduino/minilcd/minilcd.ino
+++ Code/arduino/minilcd/minilcd.ino
@@ -2,57 +2,68 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 
-// Pin mapping ESP32 → ST7789V
-#define TFT_CS    15    // D15
-#define TFT_DC     2    // D2
-#define TFT_RST    4    // D4
-#define TFT_MOSI  23    // D23
-#define TFT_SCLK  18    // D18
-#define TFT_BL    32    // D32 (backlight)
+#define TFT_CS    15
+#define TFT_DC     2
+#define TFT_RST    4
+#define TFT_MOSI  23
+#define TFT_SCLK  18
+#define TFT_BL    32
 
-// Oggetto display
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
-// Dimensioni display
 #define TFT_WIDTH  172
 #define TFT_HEIGHT 320
 
 void setup() {
-  // Reset hardware
   pinMode(TFT_RST, OUTPUT);
   digitalWrite(TFT_RST, LOW);
   delay(50);
   digitalWrite(TFT_RST, HIGH);
   delay(150);
 
-  // Inizializza SPI
   SPI.begin(TFT_SCLK, -1, TFT_MOSI);
-
-  // Inizializza display
   tft.init(TFT_WIDTH, TFT_HEIGHT);
-  tft.setRotation(0);
 
-  // Accendi backlight
+  // Ruota display di 90° CW
+  tft.setRotation(1);
+
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);
 
-  // Pulizia completa: fillScreen bianco
   tft.fillScreen(ST77XX_WHITE);
-  delay(100);
 
-  // Sovrascrivi tutta la RAM con scacchiera a blocchi
-  int blockSize = 10; // dimensione quadrato scacchiera
-  for (int y = 0; y < TFT_HEIGHT; y += blockSize) {
-    for (int x = 0; x < TFT_WIDTH; x += blockSize) {
-      if (((x/blockSize) + (y/blockSize)) % 2 == 0) {
-        tft.fillRect(x, y, blockSize, blockSize, ST77XX_RED);
-      } else {
-        tft.fillRect(x, y, blockSize, blockSize, ST77XX_BLUE);
-      }
-    }
-  }
+  String line1 = "Hello";
+  String line2 = "World";
+
+  int size1 = 2;
+  int size2 = 3;
+
+  // Dopo la rotazione: scambiamo logica width/height
+  int screenWidth = TFT_HEIGHT;  // effettiva larghezza fisica
+  int screenHeight = TFT_WIDTH;  // effettiva altezza fisica
+
+  // Larghezza testo
+  int w1 = line1.length() * 6 * size1;
+  int w2 = line2.length() * 6 * size2;
+
+  // Altezza testo
+  int h1 = 8 * size1;
+  int h2 = 8 * size2;
+  int totalHeight = h1 + h2 + 5;
+
+  int x1 = (screenWidth - w1) / 2;
+  int x2 = (screenWidth - w2) / 2;
+  int y1 = (screenHeight - totalHeight) / 2;
+  int y2 = y1 + h1 + 5;
+
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setCursor(x1, y1);
+  tft.setTextSize(size1);
+  tft.print(line1);
+
+  tft.setCursor(x2, y2);
+  tft.setTextSize(size2);
+  tft.print(line2);
 }
 
-void loop() {
-  // Nessuna azione, scacchiera fissa
-}
+void loop() {}
